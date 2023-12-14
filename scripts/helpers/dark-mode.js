38737683e7e1afddb0cc556a911ac71f4680e76d
Eugene <eugenehelland@mail.com>
2023-12-14 15:02:27 -0700

    Remove tracked .js files

--- scripts/helpers/dark-mode.js
+++
@@ -1,8 +0,0 @@
-const body = document.querySelector("body");
-const darkModeIcon = document.querySelector("#dark-mode-icon");
-
-export function darkMode() {
-  darkModeIcon.addEventListener("click", () => {
-    body.classList.toggle("dark-mode");
-  });
-}
