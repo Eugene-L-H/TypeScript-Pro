51cb2b719dff6ca9bcd3c2643833d1a48e5e7504
Eugene <eugenehelland@mail.com>
2023-12-20 18:11:20 -0700

    Convert app.js to typescript.

---
+++ app.ts
@@ -0,0 +1,31 @@
+// Global state variables, and helper functions
+import {
+  darkMode,
+  updateWeather,
+  menuFunctionality,
+  tasksStorageToDisplay,
+  populateLocalProjectsArray
+} from "./scripts/imports.js";
+
+// Add page reload to title in the header.
+const siteTitle = document.querySelector("#site-title");
+if (siteTitle) {
+  siteTitle.addEventListener("click", () => {
+    location.reload();
+  });
+}
+
+// Dark mode functionality.
+darkMode();
+
+// Display the weather information.
+updateWeather();
+
+// Mobile menu functionality.
+menuFunctionality();
+
+// Check local storage for tasks.
+tasksStorageToDisplay();
+
+// Check local storage for projects.Populate local variable.
+populateLocalProjectsArray();
